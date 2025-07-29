544a70dff0f92b742859d8ed24a80b91096cea9a
Shenggao Li <qluanl.sg@gmail.com>
2025-07-28 20:36:21 -0700

    Correct a comment mistake

--- CPP/MotionMatcher_Trick.hpp
+++ CPP/MotionMatcher_Trick.hpp
@@ -4,7 +4,7 @@
 class MotionMatcher {
 public:
     size_t N;   // Length of kernel
-    size_t m;   // Number of kernels (each one represent a channel)
+    size_t m;   // Number of channels (each one represent a channel)
     unique_ptr<float[]> rk; // Duplicated rolling kernel, dim: 2Nxm
     unique_ptr<float[]> A;  // Cache matrix, dim: NxN
     unique_ptr<float[]> r;  // Result, dim: N
